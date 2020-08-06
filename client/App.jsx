cec72080da44546364b1bf5a93d4eab262cc4ef0
Blake Jones <blakemattjones@gmail.com>
2020-08-06 13:19:09 -0600

    Initial Set Up Completed

---
+++ client/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import axios from 'axios';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      datesSelected: []
+    }
+  }
+}
+
+export default App;
