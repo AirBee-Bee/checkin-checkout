9a90a2b7b7237a0b1034e176e69dd523685133f3
Blake Jones <blakemattjones@gmail.com>
2020-08-10 10:12:13 -0600

    Test Set Up Need Ideas to Test

--- test/server.test.js
+++ test/server.test.js
@@ -2,9 +2,10 @@ const Room = require('../database/index.js');
 
 describe('Database is seeded', () => {
   test('Database is seeded', () => {
-    var rooms = Room.find();
-    var length = rooms.length;
-    expect(length).toBe(100);
+    return Room.find().then(rooms => {
+      var length = rooms.length;
+      expect(length).toBe(100);
+    })
   });
 
   test('Database is mostly random', () => {
@@ -14,17 +15,18 @@ describe('Database is seeded', () => {
     } else {
       next = random + 1;
     }
-    var room1 = Room.find({uniqueId: random});
-    var room2 = Room.find({uniqueId: next});
+    return Room.find({uniqueId: random}).then(room1 => {
+      var room2 = Room.find({uniqueId: next});
 
-    var sameValues = [];
-    for (var keys in room1[0]) {
+      var sameValues = [];
+      for (var keys in room1[0]) {
 
-        if (room1[0][keys] === room2[0][keys]) {
-          sameValues.push({keys: room1[0][keys]})
-        }
+          if (room1[0][keys] === room2[0][keys]) {
+            sameValues.push({keys: room1[0][keys]})
+          }
 
-    }
-    return sameValues;
+      }
+      expect(sameValues.length).toBeLessThan(6);
+    })
   })
 })
